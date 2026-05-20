833b57818be61b2dd96b00325532e13ef518e93f
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-05-20 22:21:25 +0300

    Created and improved components of the reports page with new things and a better UI

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,67 +1,43 @@
 import MainLayout from "../layouts/MainLayout";
-import Sidebar from "../components/shared/SideBar";
+
 import Navbar from "../components/shared/Navbar";
 
-import ReportSummaryCard from "../components/reports/ReportSummaryCard";
-import TradingJournalPanel from "../components/reports/TradingJournalpanel";
-import AIReportPanel from "../components/reports/AIReportPanel";
-import ExportReportsPanel from "../components/reports/ExportReportsPanel";
+import ReportsOverviewPanel from "../components/reports/ReportsOverviewPanel";
+import PerformanceReportPanel from "../components/reports/PerformanceReportPanel";
+import RiskReportPanel from "../components/reports/RiskReportPanel";
+import AIReportInsightsPanel from "../components/reports/AIReportInsightsPanel";
 
 function Reports() {
   return (
     <MainLayout>
 
-      <Sidebar />
+      <div className="min-h-screen bg-slate-950">
 
-      <div className="flex-1 bg-[#0B1120] p-6 overflow-y-auto">
-
+        {/* Navbar */}
         <Navbar />
 
-        {/* Summary Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 mb-8">
+        {/* Reports Content */}
+        <div
+          className="
+            space-y-8
 
-          <ReportSummaryCard
-            title="Monthly ROI"
-            value="+18.4%"
-            color="text-green-400"
-          />
+            p-4
+            md:p-6
+            xl:p-8
+          "
+        >
 
-          <ReportSummaryCard
-            title="Total Trades"
-            value="148"
-            color="text-blue-400"
-          />
+          {/* Reports Overview */}
+          <ReportsOverviewPanel />
 
-          <ReportSummaryCard
-            title="Win Rate"
-            value="71%"
-            color="text-yellow-400"
-          />
+          {/* Performance Reports */}
+          <PerformanceReportPanel />
 
-          <ReportSummaryCard
-            title="Net Profit"
-            value="$12,840"
-            color="text-green-400"
-          />
+          {/* Risk Reports */}
+          <RiskReportPanel />
 
-        </div>
-
-        {/* Main Reports Grid */}
-        <div className="grid grid-cols-1 xl:grid-cols-3 gap-5">
-
-          <div className="xl:col-span-2 space-y-5">
-
-            <TradingJournalPanel />
-
-            <AIReportPanel />
-
-          </div>
-
-          <div>
-
-            <ExportReportsPanel />
-
-          </div>
+          {/* AI Report Insights */}
+          <AIReportInsightsPanel />
 
         </div>
 
