cba256c0b45c3155a71dd99b93185fba0852b851
Ric Musyoki <ric.musyoki@student.moringaschool.com>
2026-06-01 16:38:43 +0300

    Setup backend architecture and fastapi foundation

---
+++ backend/app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Rick Research Lab"
+
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings() 
